742bc2d45d5a70d01ed6a9c365e18a672d3d265c
WWWJADE <1617459108@qq.com>
2024-08-23 16:26:46 +0800

    try to push test on git;

--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
-
+#include<vector>
+#include<string>
 using namespace std;
 
 int main(){
+    string str = "learning how to use code";
     cout<<"hello world"<<endl;
+    cout<<str<<endl;
     return 0;
 }
